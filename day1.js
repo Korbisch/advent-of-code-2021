8500eafaa515aae4822eb05d7f7374ec97193b7f
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2021-12-02 20:27:09 +0100

    day 1

--- day1.js
+++ day1.js
@@ -1,18 +1,34 @@
 const fs = require('fs')
-
+const inputPath = '/Users/korbinianschleifer/desktop/advent-of-code-2021/inputs/'
 const filename = 'day1.txt'
 
-fs.readFile('/Users/korbinianschleifer/desktop/advent-of-code-2021/inputs/' + filename, 'utf8' , (err, data) => {
+fs.readFile( inputPath + filename, 'utf8' , (err, data) => {
   if (err) {
     console.error(err)
     return
   }
-  data = data.split("\n")
-  increased_counter = 0
-  for (let i = 1; i < data.length; i++ ) {
-      if (data[i-1] > data[i]) {
-          increased_counter++
-      }
+  data = data.split("\n").map(Number);
+
+  // Part 1
+  numberOfSingleIncreases = countNumberOfIncreases(data)
+  console.log("first solution: " + numberOfSingleIncreases)
+
+  // Part 2
+  threeMeasurements = []
+  for (let i = 2; i < data.length; i++) {
+      threeMeasurements.push(data[i-2] + data[i-1] + data[i])
   }
-  console.log("first solution:" increased_counter)
+
+  secondSolution = countNumberOfIncreases(threeMeasurements)
+  console.log("second solution: " + secondSolution)
 })
+
+function countNumberOfIncreases(arr) {
+    result = 0
+    for (let i = 1; i < arr.length; i++ ) {
+        if (arr[i-1] < arr[i]) {
+            result++
+        }
+    }
+    return result
+}
