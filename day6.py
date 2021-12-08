d0169ea9531415a8cb0246c2179da1b905848e92
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2021-12-08 20:57:03 +0100

    day 6

--- day6.py
+++ day6.py
@@ -1,5 +1,4 @@
-import copy
-file_path = '/Users/korbinianschleifer/desktop/advent-of-code-2021/inputs/day6.txt'
+file_path = './inputs/day6.txt'
 
 input_row = []
 
@@ -10,6 +9,7 @@ with open(file_path, 'r') as file:
         input_row = [int(val) for val in input_row]
 
 ### Part 1 ###
+# saving all the fish in a dictionary {0: count, 1: count, ...}
 current_nums = dict()
 for i in range(9):
     current_nums[i] = input_row.count(i)
@@ -20,16 +20,12 @@ def decrement_days_get_zeros(nums):
         nums[i] = nums[i + 1]
     nums[6] += zero_count
     nums[8] = 0
-    return nums, zero_count
-
-def create_new_fish(zero_count, current_nums):
-    current_nums[8] = zero_count
-    return current_nums
+    return zero_count
 
 # iteration
 for _ in range(80):
-    current_nums, zero_count = decrement_days_get_zeros(current_nums)
-    current_nums = create_new_fish(zero_count, current_nums)
+    zero_count = decrement_days_get_zeros(current_nums)
+    current_nums[8] = zero_count
 
 solution1 = 0
 for i in range(9):
@@ -39,8 +35,8 @@ print(solution1)
 
 ### Part 2 ###
 for _ in range(256-80):
-    current_nums, zero_count = decrement_days_get_zeros(current_nums)
-    current_nums = create_new_fish(zero_count, current_nums)
+    zero_count = decrement_days_get_zeros(current_nums)
+    current_nums[8] = zero_count
 
 solution2 = 0
 for i in range(9):
